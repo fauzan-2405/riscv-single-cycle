6458da83e8e6b43b1ad6d1aaa0f628e77d55e18a
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-12-15 13:06:10 +0700

    updated the used registers

--- #1 Module/test_vector.asm
+++ #1 Module/test_vector.asm
@@ -1,16 +1,20 @@
-    ADDI x1, x0, 1      # i = 1
-    ADDI x2, x0, 0      # sum = 0
-    ADDI x3, x0, 11     # stop when i == 11
+.text
+.globl main
+main:
+    ADDI x5, x0, 1      # i = 1
+    ADDI x6, x0, 0      # sum = 0
+    ADDI x7, x0, 11     # stop when i == 11
 
 loop:
-    BLT x1, x3, body    # if i < 11, run body, else exit
+    BLT  x5, x7, body    # if i < 11, run body, else exit
     ADDI x0, x0, 0      # exit (NOP)
     # nothing else needed here
 
 body:
-    ADD  x2, x2, x1     # sum += i
-    ADDI x1, x1, 1      # i++
-    BLT  x1, x3, loop   # loop again if i < 11
+    ADD  x6, x6, x5     # sum += i
+    ADDI x5, x5, 1      # i++
+    BLT  x5, x7, loop   # loop again if i < 11
 
 exit:
-    ADDI x0, x0, 0      # return 0
+    li a0, 10      # return 0
+    ecall
