d60030c5b7e8a6b173509d8e33520b53e0a41d76
fauzan-2405 <fauzaanmuhammaad@gmail.com>
2025-12-16 13:40:21 +0700

    change the ALTSYNCRAM to BRAM

--- #1 Module/instr_rom_rv32i.v
+++ #1 Module/instr_rom_rv32i.v
@@ -1,24 +1,30 @@
 module instr_rom_rv32i (
-    input wire clock, // diperlukan untuk ALTSYNCRAM
-    input wire [31:0] PC, // byte address
+    input wire clock, 
+    input wire [31:0] PC, 
     output wire [31:0] INSTR
 );
     // Word index untuk 32 word
     wire [4:0] waddr = PC[6:2];
-    wire inv_clock = ~clock;
 
-    altsyncram #(
-        .operation_mode("ROM"),
-        .width_a(32),
-        .widthad_a(5), // 32 word
-        .init_file("imemory_rv32i.mif"),
-        .outdata_reg_a("UNREGISTERED")
-    ) rom (
-        .clock0 (inv_clock), // pembacaan secara efektif terjadi saat falling
-        .address_a(waddr),
-        .q_a (INSTR),
-        .wren_a (1'b0),
-        .data_a (32'b0)
+    xpm_memory_sprom #(
+        .ADDR_WIDTH_A        (5),         // 32 words / $clog2(32)
+        .MEMORY_SIZE         (32*32),     // bits
+        .READ_DATA_WIDTH_A   (32),
+        .READ_LATENCY_A      (1),         // synchronous read (1 cycle)
+        .MEMORY_INIT_FILE    ("imemory_rv32i.mem"),
+        .MEMORY_PRIMITIVE    ("auto"),    // let Vivado choose BRAM/LUTRAM
+        .ECC_MODE            ("no_ecc"),
+        .AUTO_SLEEP_TIME     (0),
+        .WAKEUP_TIME         ("disable")
+    ) instr_rom (
+        .clka   (clock),
+        .addra  (waddr),
+        .ena    (1'b1),
+        .rsta   (1'b0),
+        .regcea (1'b1),
+        .douta  (INSTR),
+        .injectsbiterra (1'b0),
+        .injectdbiterra (1'b0)
     );
     
 endmodule
